871049878ea8c7f7c430766f2e1ebc76b3151dde
Matthias DV <devriendt.matthias@gmail.com>
2015-06-08 13:30:03 +0200

    Applying three-dimensional noise on a sphere, tweakable

--- coffeescript/main.coffee
+++ coffeescript/main.coffee
@@ -1,3 +1,10 @@
+cfg = {
+  NOISE_AMPLITUDE: 5
+  POINTS: false
+  WIREFRAME: true
+  POLY_DETAIL: 3
+}
+
 # Include FPS and Render stats 
 stats = new Stats()
 stats.domElement.style.position = 'absolute'
@@ -12,7 +19,75 @@ rendererStats.domElement.style.bottom   = '0px'
 # Generate a WebGLrenderer instance
 renderer = new THREE.WebGLRenderer()
 
-# The actal boilerplate part
+
+# Todo, this object is a temporary solution
+
+noiseOptions = {
+  amplitude: 1.0
+  frequency: 1.0
+  octaves: 1
+  persistence: 0.5
+}
+
+
+complexGeometry = {}
+
+complexGeometry.initiateNoise = (options) ->
+  new FastSimplexNoise(options)
+
+complexGeometry.geometry = (diameter, detail) ->
+  d = diameter
+  c = d * Math.PI
+  r = d / 2
+
+  new THREE.IcosahedronGeometry(d, detail)
+
+complexGeometry.sample = (geometry, noise) -> #double arrow?
+  console.assert(geometry.vertices?)
+  for v, i in geometry.vertices
+    e = noise.getSpherical3DNoise( 1 * Math.PI, v.x, v.y, v.z) #TODO: hardcode c
+
+    v.multiplyScalar( 1 + e / cfg.NOISE_AMPLITUDE )
+
+  geometry.verticesNeedUpdate = true
+
+
+complexGeometry.new = ->
+
+  # Remove existing objects from the scene
+  for i in demo.scene.children
+    demo.scene.remove(i) if i.type == "Mesh"
+  
+  # Initiate a new noise field
+  noise = complexGeometry.initiateNoise(noiseOptions)
+
+  # Generate base geometry
+  geometry = complexGeometry.geometry(1, cfg.POLY_DETAIL)
+
+  # Sample noise and distort
+  complexGeometry.sample(geometry, noise)
+
+  # Create a new mesh and add to scene
+
+  if cfg.POINTS
+    material = new THREE.PointCloudMaterial { size: 1, sizeAttenuation: false }
+    @mesh = new THREE.PointCloud( geometry, material)
+  else
+    if cfg.WIREFRAME
+      material = new THREE.MeshBasicMaterial { color: 0xffffff, wireframe: true }
+    else
+      material = new THREE.MeshPhongMaterial { color: 0xffffff, shading: THREE.FlatShading }
+    @mesh = new THREE.Mesh( geometry, material)
+    
+  
+  @mesh.castShadow = true
+  @mesh.receiveShadow = true
+  demo.scene.add(@mesh)
+
+
+
+
+# The actual boilerplate part
 demo = Sketch.create({
 
   type: Sketch.WEBGL
@@ -22,15 +97,14 @@ demo = Sketch.create({
   setup: ->
 
     @camera = new THREE.PerspectiveCamera(45, @.width / @.height, 1, 10000 )
-    @camera.position.z = 1000
+    @camera.position.set(0, 0, 4)
+
+    @light = new THREE.PointLight( 0xffffff )
+    @light.position.set(500, 1000,1000) 
 
     @scene = new THREE.Scene()
 
-    geometry = new THREE.CubeGeometry( 200, 200, 200)
-    material = new THREE.MeshBasicMaterial { color: 0xFFFFFF, wireframe: true }
-    @mesh = new THREE.Mesh( geometry, material )
-
-    @scene.add(@mesh)
+    @scene.add(@light)
 
   resize: ->
     @camera.aspect = @.width / @.height
@@ -43,9 +117,11 @@ demo = Sketch.create({
     ## Start of stats.js monitored code.
     stats.begin()
 
-    @mesh.rotation.x += 0.01
-    @mesh.rotation.y += 0.02
-
+    if @scene.children? && @scene.children.length > 0
+      for mesh in @scene.children
+        mesh.rotation.x += 0.002
+        # mesh.rotation.y += 0.005
+      
     renderer.render( @scene, @camera )
 
     ## End of stats.js monitored code.
@@ -55,6 +131,10 @@ demo = Sketch.create({
     rendererStats.update(renderer)
   })
 
+
+
+
+
 window.onload = ->
 
   # Append stats indicators to the dom
@@ -63,3 +143,38 @@ window.onload = ->
 
   gui = new dat.GUI()
 
+  # Create the initial mesh
+  complexGeometry.new()
+
+  # Set the noise field's options as tunable variables 
+  gui.add(noiseOptions, "amplitude", -10, 1000).onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(noiseOptions, "frequency", -10, 100).onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(noiseOptions, "octaves").onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(noiseOptions, "persistence", 0.5, 1).onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(cfg, "NOISE_AMPLITUDE", 1, 10).onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(cfg, "POINTS").onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(cfg, "WIREFRAME").onChange( ->
+    complexGeometry.new()
+    )
+
+  gui.add(cfg, "POLY_DETAIL").onChange( ->
+    complexGeometry.new()
+    )
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var demo, renderer, rendererStats, stats;
+var cfg, complexGeometry, demo, noiseOptions, renderer, rendererStats, stats;
+
+cfg = {
+  NOISE_AMPLITUDE: 5,
+  POINTS: false,
+  WIREFRAME: true,
+  POLY_DETAIL: 3
+};
 
 stats = new Stats();
 
@@ -18,22 +25,87 @@ rendererStats.domElement.style.bottom = '0px';
 
 renderer = new THREE.WebGLRenderer();
 
+noiseOptions = {
+  amplitude: 1.0,
+  frequency: 1.0,
+  octaves: 1,
+  persistence: 0.5
+};
+
+complexGeometry = {};
+
+complexGeometry.initiateNoise = function(options) {
+  return new FastSimplexNoise(options);
+};
+
+complexGeometry.geometry = function(diameter, detail) {
+  var c, d, r;
+  d = diameter;
+  c = d * Math.PI;
+  r = d / 2;
+  return new THREE.IcosahedronGeometry(d, detail);
+};
+
+complexGeometry.sample = function(geometry, noise) {
+  var e, i, j, len, ref, v;
+  console.assert(geometry.vertices != null);
+  ref = geometry.vertices;
+  for (i = j = 0, len = ref.length; j < len; i = ++j) {
+    v = ref[i];
+    e = noise.getSpherical3DNoise(1 * Math.PI, v.x, v.y, v.z);
+    v.multiplyScalar(1 + e / cfg.NOISE_AMPLITUDE);
+  }
+  return geometry.verticesNeedUpdate = true;
+};
+
+complexGeometry["new"] = function() {
+  var geometry, i, j, len, material, noise, ref;
+  ref = demo.scene.children;
+  for (j = 0, len = ref.length; j < len; j++) {
+    i = ref[j];
+    if (i.type === "Mesh") {
+      demo.scene.remove(i);
+    }
+  }
+  noise = complexGeometry.initiateNoise(noiseOptions);
+  geometry = complexGeometry.geometry(1, cfg.POLY_DETAIL);
+  complexGeometry.sample(geometry, noise);
+  if (cfg.POINTS) {
+    material = new THREE.PointCloudMaterial({
+      size: 1,
+      sizeAttenuation: false
+    });
+    this.mesh = new THREE.PointCloud(geometry, material);
+  } else {
+    if (cfg.WIREFRAME) {
+      material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        wireframe: true
+      });
+    } else {
+      material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        shading: THREE.FlatShading
+      });
+    }
+    this.mesh = new THREE.Mesh(geometry, material);
+  }
+  this.mesh.castShadow = true;
+  this.mesh.receiveShadow = true;
+  return demo.scene.add(this.mesh);
+};
+
 demo = Sketch.create({
   type: Sketch.WEBGL,
   element: renderer.domElement,
   context: renderer.context,
   setup: function() {
-    var geometry, material;
     this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 10000);
-    this.camera.position.z = 1000;
+    this.camera.position.set(0, 0, 4);
+    this.light = new THREE.PointLight(0xffffff);
+    this.light.position.set(500, 1000, 1000);
     this.scene = new THREE.Scene();
-    geometry = new THREE.CubeGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({
-      color: 0xFFFFFF,
-      wireframe: true
-    });
-    this.mesh = new THREE.Mesh(geometry, material);
-    return this.scene.add(this.mesh);
+    return this.scene.add(this.light);
   },
   resize: function() {
     this.camera.aspect = this.width / this.height;
@@ -41,9 +113,15 @@ demo = Sketch.create({
     return renderer.setSize(this.width, this.height);
   },
   draw: function() {
+    var j, len, mesh, ref;
     stats.begin();
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
+    if ((this.scene.children != null) && this.scene.children.length > 0) {
+      ref = this.scene.children;
+      for (j = 0, len = ref.length; j < len; j++) {
+        mesh = ref[j];
+        mesh.rotation.x += 0.002;
+      }
+    }
     renderer.render(this.scene, this.camera);
     stats.end();
     return rendererStats.update(renderer);
@@ -54,7 +132,32 @@ window.onload = function() {
   var gui;
   document.body.appendChild(stats.domElement);
   document.body.appendChild(rendererStats.domElement);
-  return gui = new dat.GUI();
+  gui = new dat.GUI();
+  complexGeometry["new"]();
+  gui.add(noiseOptions, "amplitude", -10, 1000).onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(noiseOptions, "frequency", -10, 100).onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(noiseOptions, "octaves").onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(noiseOptions, "persistence", 0.5, 1).onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(cfg, "NOISE_AMPLITUDE", 1, 10).onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(cfg, "POINTS").onChange(function() {
+    return complexGeometry["new"]();
+  });
+  gui.add(cfg, "WIREFRAME").onChange(function() {
+    return complexGeometry["new"]();
+  });
+  return gui.add(cfg, "POLY_DETAIL").onChange(function() {
+    return complexGeometry["new"]();
+  });
 };
 
 //# sourceMappingURL=main.js.map
